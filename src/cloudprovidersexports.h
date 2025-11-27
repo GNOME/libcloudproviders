aa73c3847236c4684a90f33c6fd2f095e05b5548
Chun-wei Fan <fanc999@yahoo.com.tw>
2025-11-27 18:20:06 +0800

    src/: Add header to define LIBCLOUDPROVIDERS_API
    This macro can be overriden to export symbols for the library, which
    will be done in a later commit.

---
+++ src/cloudprovidersexports.h
@@ -0,0 +1,27 @@
+/* cloudprovidersaccount.h
+ *
+ * Copyright (C) 2015 Carlos Soriano <csoriano@gnome.org>
+ * Copyright (C) 2017 Julius Haertl <jus@bitgrid.net>
+ *
+ * This file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CLOUD_PROVIDERS_EXPORT_H
+#define CLOUD_PROVIDERS_EXPORT_H
+
+#ifndef LIBCLOUDPROVIDERS_API
+# define LIBCLOUDPROVIDERS_API extern
+#endif
+
+#endif /* CLOUD_PROVIDERS_EXPORT_H */
